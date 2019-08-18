b6cb60e0d674ff2347b3d98eb6151e1fc257b5b9
Ethan Merrill <eth22mer@gmail.com>
2019-08-18 07:24:24 -0400

    Broken. Need a way to make driver global

--- webscraper.js
+++ webscraper.js
@@ -1,17 +1,56 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 var webdriver = require('selenium-webdriver');
-async function example(username, password) {
-    let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://cloud.3dprinteros.com");
-    await driver.findElement(webdriver.By.xpath('//*[@id="signinUsername"]')).sendKeys(username)
-    driver.findElement(webdriver.By.xpath('//*[@id="signinPassword"]')).sendKeys(password)
-    await driver.findElement(webdriver.By.xpath('//*[@id="signIn"]/form/div[4]/input')).sendKeys(Key.ENTER)
-    await driver.quit()
+let driver;
+const { BeforeAll, AfterAll, setWorldConstructor } = require('cucumber');
+const seleniumWebdriver = require('selenium-webdriver');
+
+function build_driver() {
+    driver = new seleniumWebdriver.Builder()
+        .forBrowser('chrome')
+        .build();
+    setWorldConstructor(World.bind(undefined, driver));
 }
 
-function setup() {
+
+function World(driver) {
+    this.driver = driver;
+}
+
+async function login(driver) {
+    /*let driver = new Builder().forBrowser("chrome").build();*/
     var keys = require("C:/Users/eth22/Documents/GitHub/Selenium_JS_webscraping/keys.json")
-    example(keys.username, keys.password)
+    try {
+        await driver.get("https://cloud.3dprinteros.com/printing/");
+        await driver.findElement(webdriver.By.xpath('//*[@id="signinUsername"]')).sendKeys(keys.username)
+        await driver.findElement(webdriver.By.xpath('//*[@id="signinPassword"]')).sendKeys(keys.password)
+        await driver.findElement(webdriver.By.xpath('//*[@id="signIn"]/form/div[4]/input')).sendKeys(Key.ENTER)
+        await driver.get("https://cloud.3dprinteros.com/printing/");
+    } finally {
+        return driver;
+    }
+
+    /*await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath('//*[@id="host_id_25197"]/div[3]'))) */
 }
 
-setup();
+function remove_objects(driver) {
+    driver.findElement(webdriver.By.xpath('//*[text()[contains(.,"anavarroaguayo@wpi.edu")]]'))
+    driver.quit
+}
+
+function sleep(millisecondsToWait) {
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + millisecondsToWait) {
+        /* do nothing; this will exit once it reaches the time limit */
+        /* if you want you could do something and exit */
+    }
+}
+
+function main() {
+
+    driver = build_driver;
+    /*driver = login(driver);*/
+    sleep(1000);
+    remove_objects(driver);
+}
+
+main();
