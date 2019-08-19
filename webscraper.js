c95cfcf640220b8379afcce312d1f501b89c0b1c
Ethan Merrill <eth22mer@gmail.com>
2019-08-19 18:50:10 -0400

    Altered construction of driver

--- webscraper.js
+++ webscraper.js
@@ -1,30 +1,28 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 var webdriver = require('selenium-webdriver');
-let driver;
-const { BeforeAll, AfterAll, setWorldConstructor } = require('cucumber');
+/*const { BeforeAll, AfterAll, setWorldConstructor } = require('cucumber');
 const seleniumWebdriver = require('selenium-webdriver');
-
+/*
 function build_driver() {
     driver = new seleniumWebdriver.Builder()
-        .forBrowser('chrome')
-        .build();
+
     setWorldConstructor(World.bind(undefined, driver));
 }
+*/
 
+var driver = new webdriver.Builder().
+withCapabilities(webdriver.Capabilities.chrome()).
+build();
 
-function World(driver) {
-    this.driver = driver;
-}
-
-async function login(driver) {
+function login(driver) {
     /*let driver = new Builder().forBrowser("chrome").build();*/
     var keys = require("C:/Users/eth22/Documents/GitHub/Selenium_JS_webscraping/keys.json")
     try {
-        await driver.get("https://cloud.3dprinteros.com/printing/");
-        await driver.findElement(webdriver.By.xpath('//*[@id="signinUsername"]')).sendKeys(keys.username)
-        await driver.findElement(webdriver.By.xpath('//*[@id="signinPassword"]')).sendKeys(keys.password)
-        await driver.findElement(webdriver.By.xpath('//*[@id="signIn"]/form/div[4]/input')).sendKeys(Key.ENTER)
-        await driver.get("https://cloud.3dprinteros.com/printing/");
+        driver.get("https://cloud.3dprinteros.com/printing/");
+        driver.findElement(webdriver.By.xpath('//*[@id="signinUsername"]')).sendKeys(keys.username);
+        driver.findElement(webdriver.By.xpath('//*[@id="signinPassword"]')).sendKeys(keys.password);
+        driver.findElement(webdriver.By.xpath('//*[@id="signIn"]/form/div[4]/input')).sendKeys(Key.ENTER);
+        driver.get("https://cloud.3dprinteros.com/printing/");
     } finally {
         return driver;
     }
@@ -32,10 +30,7 @@ async function login(driver) {
     /*await driver.wait(webdriver.until.elementLocated(webdriver.By.xpath('//*[@id="host_id_25197"]/div[3]'))) */
 }
 
-function remove_objects(driver) {
-    driver.findElement(webdriver.By.xpath('//*[text()[contains(.,"anavarroaguayo@wpi.edu")]]'))
-    driver.quit
-}
+
 
 function sleep(millisecondsToWait) {
     var now = new Date().getTime();
@@ -45,12 +40,20 @@ function sleep(millisecondsToWait) {
     }
 }
 
-function main() {
-
-    driver = build_driver;
-    /*driver = login(driver);*/
-    sleep(1000);
-    remove_objects(driver);
+function remove_objects(driver) {
+    try {
+        driver.wait(function() {
+            return driver.executeScript('return document.readyState').then(function(readyState) {
+                return readyState === 'complete';
+            });
+        });
+        driver.findElement(webdriver.By.xpath('//*[text()[contains(.,"anavarroaguayo@wpi.edu")]]'));
+    } finally {
+        driver.quit
+    }
 }
 
-main();
+
+login(driver);
+sleep(1000);
+remove_objects(driver);
